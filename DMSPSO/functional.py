32505e7d8a967b75cc53d41ed84da06a87e6031a
SioKCronin <siobhankcronin@gmail.com>
2018-03-16 11:40:04 -0700

    Clean up

--- DMSPSO/functional.py
+++ DMSPSO/functional.py
@@ -1,7 +1,6 @@
 # Functional PSO
 
 import numpy as np
-from scipy.spatial import KDTree
 
 m = 3 # Each swarm's population size (2, 3, 5 tested)
 n = 10 # Number of swarms (10, 20, 50 tested)
@@ -10,15 +9,15 @@ c2 = 0.3
 w = 0.9
 p = 2
 k = 1
-R = 10
-max_gen = 100
+R = 5
+iters = 2000
 dimensions = 2
 
 pos = []
 personal_best_pos = []
 pbest_cost = []
-best_cost = []
-best_pos = [[0, 0]]
+best_cost = [5]
+best_pos = [0, 0]
 velocity = []
 
 for i in range(n):
@@ -39,9 +38,12 @@ for i in range(n):
     pbest_cost.append(subswarm_pbest_cost)
     velocity.append(subswarm_velocity)
 
-i = 1
+x = 0
+# LSPO for the first 0.9 iterations
+while x < (0.9 * iters):
 
-while i < (0.9 * max_gen):
+    if (x % R)== 0:
+        pass
 
     for i in range(n):
         for j in range(m):
@@ -50,43 +52,47 @@ while i < (0.9 * max_gen):
             if current_cost < personal_best_cost:
                 personal_best_pos[i][j] = pos[i][j]
                 pbest_cost[i][j] = current_cost
+
+            # Update swarm global best
             if personal_best_cost < best_cost:
-                best_cost = pbest_cost[i][j]
+                best_cost = personal_best_cost
                 best_pos = personal_best_pos[i][j]
 
 	    # Compute for cognitive and social terms
             cognitive = (c1 * np.random.uniform(0, 1, 2)) * (personal_best_pos[i][j] - pos[i][j])
             social = (c2 * np.random.uniform(0, 1, 2)) * (best_pos - pos[i][j])
-            velocity = w + cognitive + social
+            velocity[i][j] = (w * velocity[i][j]) + cognitive + social
 
             # Update positions
-
             pos += velocity
 
-    i += 1
+    x += 1
 
-while i < (0.1 * max_gen):
+x = 0
+# GPSO for the last 0.1 iterations
+while x < (0.1 * iters):
     for i in range(n):
         for j in range(m):
             current_cost = sum([i**2 for i in (pos[i][j])])
             pbest_cost = sum([i**2 for i in (personal_best_pos[i][j])])
             if current_cost < pbest_cost:
                 best_pos[i][j] = pos[i][j]
-                best_cost[i][j] = current_cost
+                best_cost = current_cost
 
+            # Update global best (across all swarms) personal best is better
             if np.min(pbest_cost) < best_cost:
+                print("pbest", np.min(pbest_cost))
                 best_cost = np.min(pbest_cost)
                 best_pos = personal_best_pos[np.argmin(pbest_cost)]
 
 	    # Compute for cognitive and social terms
-            cognitive = (c1 * np.random.uniform(0, 1, n) * (personal_best_pos - pos))
-            social = (c2 * np.random.uniform(0, 1, n) * (best_pos - pos))
-            velocity = (w * velocity) + cognitive + social
+            cognitive = (c1 * np.random.uniform(0, 1, 2) * (personal_best_pos[i][j] - pos[i][j]))
+            social = (c2 * np.random.uniform(0, 1, 2) * (best_pos - pos[i][j]))
+            velocity[i][j] = (w * velocity[i][j]) + cognitive + social
 
             # Update positions
-
             pos += velocity
 
-    i += 1
+    x += 1
 
-print(best_pos)
+print(best_cost)
