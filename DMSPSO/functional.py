7afb6cbf8f304fb1e1af63e65a998af7f4a1213c
SioKCronin <siobhankcronin@gmail.com>
2018-03-19 18:02:49 -0700

    Namespace refactor

--- DMSPSO/functional.py
+++ DMSPSO/functional.py
@@ -2,37 +2,44 @@
 
 import numpy as np
 
+n = 10 # Number of groups in swarm (10, 20, 50 tested)
 m = 3 # Each swarm's population size (2, 3, 5 tested)
-n = 10 # Number of swarms (10, 20, 50 tested)
-c1 = 0.5
-c2 = 0.3
-w = 0.9
-p = 2
-k = 1
-R = 5
-iters = 2000
 dimensions = 2
+c1 = 0.5 # Cognitive weight (how much each particle references their memory)
+c2 = 0.3 # Social weight (how much each particle references swarm/metaswarm memory)
+w = 0.9 # Velocity weight
+R = 5 # Swarm reshuffling interval
+iters = 2000 
 
-metaswarm = []
-global_best_pos = [0, 0]
-global_best_cost = [5]
+swarm = []
+swarm_best_pos = [0, 0]
+swarm_best_cost = [5]
 
-swarm_data = []
+group_data = []
 
-for i in range(n):
-    swarm = []
-    swarm_best_pos = np.array([0, 0])
-    swarm_best_cost = 5
-    swarm_data.append([swarm_best_pos, swarm_best_cost])
+P_POS_IDX = 0
+P_VELOCITY_IDX = 1
+P_BEST_POS_IDX = 2
+P_BEST_COST_IDX = 3
 
-    for i in range(m):
-        position = np.random.uniform(-1, 1, dimensions)
+GROUP_BEST_POS_IDX = 0
+GROUP_BEST_COST_IDX = 1
+
+for group in range(n):
+    group = []
+    group_best_pos = np.array([0, 0])
+    group_best_cost = 5
+    group_data.append([group_best_pos, group_best_cost])
+
+    for particle in range(m):
+        pos = np.random.uniform(-1, 1, dimensions)
         velocity = np.random.uniform(-1, 1, dimensions)
-        personal_best_pos = np.random.uniform(-1, 1, dimensions)
-        personal_best_cost = 1
-        swarm.append([position, velocity, personal_best_pos, personal_best_cost])
+        p_best_pos = np.random.uniform(-1, 1, dimensions)
+        p_best_cost = 1
 
-    metaswarm.append(swarm)
+        group.append([pos, velocity, p_best_pos, p_best_cost])
+
+    swarm.append(group)
 
 x = 0
 
@@ -42,29 +49,27 @@ while x < (0.9 * iters):
     if (x % R)== 0:
         pass
 
-    for i in range(n):
+    for group in range(n):
 
-        for j in range(m):
-            current_cost = sum([i**2 for i in (metaswarm[i][j][0])])
-            personal_best_cost = metaswarm[i][j][3]
+        for particle in range(m):
+            current_cost = sum([i**2 for i in (swarm[group][particle][P_POS_IDX])])
+            personal_best_cost = swarm[group][particle][P_BEST_COST_IDX]
             if current_cost < personal_best_cost:
-                metaswarm[i][j][2] = metaswarm[i][j][0]
-                metaswarm[i][j][3] = current_cost
-
-            # print("Swarm data", swarm_data[i])
+                swarm[group][particle][P_BEST_POS_IDX] = swarm[group][particle][P_POS_IDX]
+                swarm[group][particle][P_BEST_COST_IDX] = current_cost
 
             # Update swarm global best
-            if personal_best_cost < swarm_data[i][1]:
-                swarm_data[i][1] = personal_best_cost
-                swarm_data[i][0] = metaswarm[i][j][2]
+            if personal_best_cost < group_data[group][GROUP_BEST_COST_IDX]:
+                group_data[group][GROUP_BEST_COST_IDX] = personal_best_cost
+                group_data[group][GROUP_BEST_POS_IDX] = swarm[group][particle][P_BEST_POS_IDX]
 
 	    # Compute velocity
-            cognitive = (c1 * np.random.uniform(0, 1, 2)) * (metaswarm[i][j][2] - metaswarm[i][j][0])
-            social = (c2 * np.random.uniform(0, 1, 2)) * (global_best_pos - metaswarm[i][j][0])
-            metaswarm[i][j][1] = (w * metaswarm[i][j][1]) + cognitive + social
+            cognitive = (c1 * np.random.uniform(0, 1, 2)) * (swarm[group][particle][P_BEST_POS_IDX] - swarm[group][particle][P_POS_IDX])
+            social = (c2 * np.random.uniform(0, 1, 2)) * (group_data[group][GROUP_BEST_POS_IDX] - swarm[group][particle][P_POS_IDX])
+            swarm[group][particle][P_VELOCITY_IDX] = (w * swarm[group][particle][P_VELOCITY_IDX]) + cognitive + social
 
-            # Update positions
-            metaswarm[i][j][0] += metaswarm[i][j][1]
+            # Update poss
+            swarm[group][particle][P_POS_IDX] += swarm[group][particle][P_VELOCITY_IDX]
 
     x += 1
 
@@ -72,27 +77,28 @@ x = 0
 
 # GPSO for the last 0.1 iterations
 while x < (0.1 * iters):
-    for i in range(n):
-        for j in range(m):
-            current_cost = sum([i**2 for i in (metaswarm[i][j][0])])
-            personal_best_cost = metaswarm[i][j][3]
+    for group in range(n):
+
+        for particle in range(m):
+            current_cost = sum([i**2 for i in (swarm[group][particle][P_POS_IDX])])
+            personal_best_cost = swarm[group][particle][P_BEST_COST_IDX]
             if current_cost < personal_best_cost:
-                metaswarm[i][j][2] = metaswarm[i][j][0]
-                metaswarm[i][j][3] = current_cost
+                swarm[group][particle][P_BEST_POS_IDX] = swarm[group][particle][P_POS_IDX]
+                swarm[group][particle][P_BEST_COST_IDX] = current_cost
 
             # Update swarm global best
-            if personal_best_cost < global_best_cost:
-                global_best_cost = personal_best_cost
-                global_best_pos = metaswarm[i][j][2]
+            if personal_best_cost < swarm_best_cost:
+                swarm_best_cost = personal_best_cost
+                swarm_best_pos = swarm[group][particle][P_BEST_POS_IDX]
 
 	    # Compute velocity
-            cognitive = (c1 * np.random.uniform(0, 1, 2)) * (metaswarm[i][j][2] - metaswarm[i][j][0])
-            social = (c2 * np.random.uniform(0, 1, 2)) * (global_best_pos - metaswarm[i][j][0])
-            metaswarm[i][j][1] = (w * metaswarm[i][j][1]) + cognitive + social
+            cognitive = (c1 * np.random.uniform(0, 1, 2)) * (swarm[group][particle][P_BEST_POS_IDX] - swarm[group][particle][P_POS_IDX])
+            social = (c2 * np.random.uniform(0, 1, 2)) * (group_data[group][GROUP_BEST_POS_IDX] - swarm[group][particle][P_POS_IDX])
+            swarm[group][particle][P_VELOCITY_IDX] = (w * swarm[group][particle][P_VELOCITY_IDX]) + cognitive + social
 
-            # Update positions
-            metaswarm[i][j][0] += metaswarm[i][j][1]
+            # Update poss
+            swarm[group][particle][P_POS_IDX] += swarm[group][particle][P_VELOCITY_IDX]
 
     x += 1
 
-print(global_best_cost)
+print(swarm_best_cost)
