1b6f19001f78bdb7e269615781efed8445e605b9
SioKCronin <siobhankcronin@gmail.com>
2018-03-22 20:51:40 -0700

    WIP

--- PSO/local_best_pso.py
+++ PSO/local_best_pso.py
@@ -1,6 +1,8 @@
 # Local Best PSO
 
 import numpy as np
+import math
+from operator import itemgetter
 from random import shuffle
 
 m = 10 # Swarm population size
@@ -8,6 +10,8 @@ dimensions = 2
 c1 = 0.5 # Cognitive weight (how much each particle references their memory)
 c2 = 0.3 # Social weight (how much each particle references swarm/metaswarm memory)
 w = 0.9 # Velocity weight
+p = 4
+k = 2
 iters = 2000 
 
 swarm = []
@@ -18,6 +22,22 @@ P_POS_IDX = 0
 P_VELOCITY_IDX = 1
 P_BEST_POS_IDX = 2
 P_BEST_COST_IDX = 3
+
+def euclideanDistance(point1, point2):
+    distance = 0
+    for x in range(dimensions):
+        distance += pow((point1[x] - point2[x]), 2)
+    return math.sqrt(distance)
+
+def getNeighbors(target, k):
+    distances = []
+    for x in range(m):
+        distances.append((swarm[x][P_POS_IDX], euclideanDistance(swarm[x][P_POS_IDX], target)))
+    sorted(distances,key=itemgetter(1))
+    neighbors = []
+    for x in range(k):
+        neighbors.append(distances[x][0])
+    return neighbors
 
 for particle in range(m):
     pos = np.random.uniform(-1, 1, dimensions)
@@ -39,14 +59,20 @@ while x < iters:
             swarm[particle][P_BEST_COST_IDX] = current_cost
 
         # Update swarm local best
-        # TODO
+        best_neighbors = getNeighbors(swarm[particle][P_BEST_POS_IDX], k+1)
+        best_cost = swarm[particle][P_BEST_COST_IDX]
+        best_pos = swarm[particle][P_BEST_POS_IDX]
+        for y in range(len(best_neighbors)):
+            if swarm[y][P_BEST_COST_IDX] < best_cost:
+                best_pos = swarm[y][P_BEST_POS_IDX]
+
         if personal_best_cost < swarm_best_cost:
             swarm_best_cost = personal_best_cost
             swarm_best_pos = swarm[particle][P_BEST_POS_IDX]
 
         # Compute velocity
         cognitive = (c1 * np.random.uniform(0, 1, 2)) * (swarm[particle][P_BEST_POS_IDX] - swarm[particle][P_POS_IDX])
-        social = (c2 * np.random.uniform(0, 1, 2)) * (swarm_best_pos - swarm[particle][P_POS_IDX])
+        social = (c2 * np.random.uniform(0, 1, 2)) * (best_pos - swarm[particle][P_POS_IDX])
         swarm[particle][P_VELOCITY_IDX] = (w * swarm[particle][P_VELOCITY_IDX]) + cognitive + social
 
         # Update poss
