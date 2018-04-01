cf0cf26afbb1d95580759830fc24dce33bb292ca
SioKCronin <siobhankcronin@gmail.com>
2018-03-31 21:03:22 -0700

    WIP

---
+++ dnpso/dnpso.py
@@ -0,0 +1,6 @@
+# Dynamic Neighborhood PSO
+
+# Update positions as usual with first objective func
+# Determine neighbors
+# Find local optima of the neighbors for the second objective func 
+# (that becomes the "local best" and will be weighted as usual)
