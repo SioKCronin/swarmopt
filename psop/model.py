93c58cfddc2a1222cd9d4d60130c6ba630063919
SioKCronin <siobhankcronin@gmail.com>
2018-04-10 16:20:08 -0700

    WIP

--- psop/model.py
+++ psop/model.py
@@ -1,3 +1,40 @@
+import json
+import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.optimizers import sgd
+from MountainCar import MountainCar
+
+class ExperienceReplay(object):
+    def __init__(self, max_memory=100, discount=0.9):
+        self.max_memory = max_memory
+        self.memory = list()
+        self.discount = discount
+
+    def remember(self, states, game_over):
+        self.memory.append([states, game_over])
+        if len(self.memory) > self.max_memory:
+            del self.memory[0]
+
+    def get_batch(self, model, batch_size=10):
+        len_memory = len(self.memory)
+        num_actions = model.output_shape[-1]
+        env_dim = self.memory[0][0][0].shape[1]
+        inputs = np.zeros((min(len_memory, batch_size), env_dim))
+        targets = np.zeros((inputs.shape[0], num_actions))
+        for i, idx in enumerate(np.random.randint(0, len_memory, size = inputs.shape[0])):
+            state_t, action_t, reward_t, state_tp1 = self.memory[idx][0]
+            game_over = self.memory[idx][1]
+
+            inputs[i:i+1] = state_t
+            targets[i] = model.predict(state_t)[0]
+            Q_sa = np.max(model.predict(state_tp1)[0])
+            if game_over:
+                targets[i, action_t] = reward_t
+            else:
+                targets[i, action_t] = reward_t + self.discount * Q_sa
+        return inputs, targets
+
 if __name__ == "__main__":
     #parameters
     epsilon = 0.1 #exploration coeff
@@ -55,7 +92,7 @@ if __name__ == "__main__":
             # adapt model
             inputs, targets = exp_replay.get_batch(model, batch_size=batch_size)
 
-            loss += model.train_on_batch(inputs, targets)[0]
+            loss += model.train_on_batch(inputs, targets)
         print("Step {} Epoch {:03d}/999 | Loss {:.4f} | Win count {}".format(step, e, loss, win_cnt))
 
     # Safe trained model weights and architecture (used for viz)
